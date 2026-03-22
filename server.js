6fddd5c9e1f282b0e9619f18ce3cc8e0553dbe7e
Caio Vinícius de Carvalho Bezerra <caio.carvalho134@hotmail.com>
2026-03-21 21:44:27 -0300

    fix: dotenv nao sobrescreve variaveis do Koyeb

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const { Pool } = require('pg');
 
 require('dotenv').config({ override: false });
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL ? process.env.DATABASE_URL.substring(0, 50) + '...' : 'NÃO DEFINIDA');
+
 const { sendLowStockEmail } = require('./services/emailService'); 
 
 // --- CONFIGURAÇÃO INICIAL ---
